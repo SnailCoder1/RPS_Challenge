788e30e8d7c192934ba59c20af71a867bc83dd8c
boa <boa.matule@gmail.com>
2019-05-09 14:18:07 +0200

    new configs

---
+++ spec.helper.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const BrowserHelpers = require('e2e_training_wheels')
+global.browser = new BrowserHelpers()
+global.expect = chai.expect;
+
+const fs = require ('fs');
+let = fs.readFileSync('/src/app.js')
+
+eval(cal + `\nexports. = `)
